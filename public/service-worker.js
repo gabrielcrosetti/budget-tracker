233d99de2926bec86fab82bc66c2a939057ede6c
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-11-18 21:48:39 -0500

    Changes to indexedDB, and service-workerjs

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,6 +22,23 @@ self.addEventListener("install", function(evt) {
     )
 });
 
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+   
+    self.clients.claim();
+  });
+
 // Set cached resources as response
 self.addEventListener("fetch", function(evt) {
     // cache requests to the API routes
